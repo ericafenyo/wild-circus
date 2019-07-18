5c6921257ea1b7c6fb6dd0e7db14ea6409e3d0ee
ericafenyo <ericafenyo@yahoo.com>
2019-07-18 08:08:49 +0200

    update code

--- app/src/App.js
+++ app/src/App.js
@@ -10,16 +10,21 @@ const events = [
     description: 'TEXT NOT NULL',
     address: 'VARCHAR(255) NOT NULL',
     image:
-      'https://images.unsplash.com/photo-1435034568314-8303dbda4b8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80',
+      'https://images.unsplash.com/photo-1475738384599-8cf3db232ffa?ixlib=rb-1.2.1&auto=format&fit=crop&w=4015&q=80',
     price: 80,
     createdAt: Date.now()
   }
 ];
 
-function App() {
+const App = () => {
   fetchEvents(events => console.log(events));
 
-  return <Events events={events} />;
+
+  return (
+    <div className="home">
+      {/* <Events events={events} />; */}
+  </div>
+  );
 }
 
 export default App;
